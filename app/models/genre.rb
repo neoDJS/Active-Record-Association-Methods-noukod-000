614cdb8ad60e3a69e81795d21dc4054ebeac61e0
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-02 17:52:03 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,8 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    Artist.name
+    Artist.all.map do |a| 
+      a.name
+    end
   end
 end
